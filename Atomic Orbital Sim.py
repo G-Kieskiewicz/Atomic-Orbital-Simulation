886fb82ba515b8c3a55e60cf6e03c9d86ed68373
Gabriel <118369896+Kiszony12@users.noreply.github.com>
2026-09-06 11:02:00 +0100

    implemented the visualiser into the sim and pyvista is acting up on my machine not sure why, the html output works though

--- Atomic Orbital Sim.py
+++ Atomic Orbital Sim.py
@@ -1,7 +1,8 @@
 import numpy as np
-import json
-from math import sqrt, exp
+# import json -- This line is useless now as i dont save the data as a json
+from math import sqrt
 import scipy.special as sp
+import pyvista as pv
 
 a0 = 1.0        # working in ratios of the Bohr's radius which is equal to (h-bar)^2/(m*k_e*e^2)
 
@@ -51,6 +52,34 @@ def sample_points(n, l, m, N=50000):
             })
     return points
 
+def plot_orbital(filename,coordinates, real_parts, point_size=0.05, background="#000000", opacity=1,
+                  positive_color="#ff8a4c", negative_color="#49c8ff"):
+    cloud = pv.PolyData(coordinates)
+
+    pos_rgb = pv.Color(positive_color).int_rgb  # hex -> (r, g, b) 0-255
+    neg_rgb = pv.Color(negative_color).int_rgb
+
+    colors = np.where(
+        real_parts[:, None] >= 0,
+        np.array(pos_rgb),
+        np.array(neg_rgb)
+    ).astype(np.uint8)
+
+    cloud["colors"] = colors
+
+    plotter = pv.Plotter()
+    plotter.set_background(background)  # hex works directly here
+    plotter.add_mesh(
+        cloud,
+        scalars="colors",
+        rgb=True,
+        point_size=point_size,
+        opacity=opacity,
+        render_points_as_spheres=False,
+    )
+    plotter.export_html(filename)
+    plotter.show()
+
 def main():
     print("=== Hydrogen Orbital Generator ===\n")
     n = int(input("Enter n (1..7): "))
@@ -61,11 +90,16 @@ def main():
     print("\nGenerating samples... please wait...")
     points = sample_points(n, l, m, N)
 
-    filename = f"orbital_n{n}_l{l}_m{m}.json"
-    with open(filename, "w") as f:
-        json.dump(points, f, indent=2)
+    filename = f"orbital_n{n}_l{l}_m{m}.html"
+    """with open(filename, "w") as f:
+        json.dump(points, f, indent=2)"""
 
-    print(f"\nSaved {len(points)} points to {filename}")
+    coordinates = np.array([[d["x coordinate"], d["y coordinate"], d["z coordinate"]] for d in points])
+    real_parts = np.array([d["wavefunction real component"] for d in points])
+
+    plot_orbital(filename, coordinates, real_parts)
+
+    print(f"\nSaved simulation to {filename}")
 
 if __name__ == "__main__":
     main()
